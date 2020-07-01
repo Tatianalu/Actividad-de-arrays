b29185f4aba32a9c4d56d0020498073a69a483ca
Tatiana <tata7_89@hotmail.com>
2020-07-01 11:42:32 -0500

    first commit

---
+++ ejemplo.js
@@ -0,0 +1,11 @@
+console.log('Hola mundo');
+
+//Array de palabra Somos.
+var arrays = [['s',1,2.5,false,'s'],
+              [true,'o',"anillo",'o',65],
+              ["sol",2.25,'m',5,false],
+              [23,'o',"Leticia",'o',43.3],
+              ['s',"mar",12,6.5,'s']];
+
+console.log(arrays[0][0],arrays[1][1],arrays[2][2],arrays[3][3],arrays[4][4]);
+console.log(arrays[0][4],arrays[1][3],arrays[2][2],arrays[3][1],arrays[4][0]);
