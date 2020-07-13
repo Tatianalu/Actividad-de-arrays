f526b7bf17b86184036242bd3803788742045995
Tatiana <tata7_89@hotmail.com>
2020-07-13 09:44:12 -0500

    Metodos array segunda parte

--- metodos-part2.js
+++ metodos-part2.js
@@ -3,5 +3,22 @@
 
 /*------- Implementación Some -------*/
 
+var animal = ["leon", "tigre", "gato", "puma"];
 
+function anicasa(arr, val){
+    return arr.some(arrVal => val === arrVal);
+}
+
+if (anicasa(animal,"leon") == 1){
+    document.write("El leon se encuentra en el zoologico");
+}
 /*------- Implementación Map -------*/
+
+var numeros = [1,2,3,4,5,6,7,8,9,10];
+var tablamul2 = numeros.map(multiplicar)
+
+function multiplicar(num){
+    return num * 2;
+}
+document.write("</br></br>Tabla del 2 </br>");
+document.write(tablamul2);
